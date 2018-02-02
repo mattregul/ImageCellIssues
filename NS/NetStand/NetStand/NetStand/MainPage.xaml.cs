18720b75c8861c0ad1e8d8f65cf2346bd8b3ecec
Matt Regul <dev.matt@pachead.com>
2018-02-02 10:16:01 -0500

    first

---
+++ NS/NetStand/NetStand/NetStand/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetStand
+{
+    class Employee
+    {
+        public UriImageSource Source { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+
+        public Employee(string _source, string _name, string _job)
+        {
+            Source = new UriImageSource
+            {
+                Uri = new Uri(_source),
+                //CachingEnabled = true,
+                //CacheValidity = new TimeSpan(0, 0, 1, 0)
+            };
+            Name = _name;
+            Job = _job;
+        }
+    }
+    public partial class MainPage : ContentPage
+	{
+        List<Employee> employeeList;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            employeeList = new List<Employee>();
+
+            // Create a few sample employees
+            for (int i = 0; i < 50; i++)
+            {
+                employeeList.Add(
+                    new Employee(
+                        "https://www.xamstatic.com/dist/images/pages/platform/visual-studio-icon-uHIZMvQl.png",
+                        "Employee " + i,
+                        "Developer"
+                    )
+                );
+            }
+
+            // Set the source
+            EmployeeListview.ItemsSource = employeeList;
+        }
+    }
+
+}
+
---
+++ PCL/XamarinForms_ListviewSelection/ListviewTestPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+
+namespace XamarinForms_ListviewSelection
+{
+    class Employee
+    {
+        public UriImageSource Source { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+
+        public Employee(string _source, string _name, string _job)
+        {
+            Source = new UriImageSource
+            {
+                Uri = new Uri(_source),
+                //CachingEnabled = true,
+                //CacheValidity = new TimeSpan(0, 0, 1, 0)
+            };
+            Name = _name;
+            Job = _job;
+        }
+    }
+
+
+    public partial class ListviewTestPage : ContentPage
+    {
+        List<Employee> employeeList;
+        public ListviewTestPage()
+        {
+            InitializeComponent();
+
+            employeeList = new List<Employee>();
+
+            // Create a few sample employees
+            for (int i = 0; i < 50; i++)
+            {
+                employeeList.Add(
+                    new Employee(
+                        "https://www.xamstatic.com/dist/images/pages/platform/visual-studio-icon-uHIZMvQl.png",
+                        "Employee " + i,
+                        "Developer"
+                    )
+                );
+            }
+
+            // Set the source
+            EmployeeListview.ItemsSource = employeeList;
+        }
+    }
+}
+
